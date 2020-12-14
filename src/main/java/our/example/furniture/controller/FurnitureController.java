390f0453583a9659a795f65a4c6b8a0ade3c3acb
wootta-1559 <wootta1559@naver.com>
2020-12-14 12:20:49 +0900

    회원가입

--- src/main/java/our/example/furniture/controller/FurnitureController.java
+++ src/main/java/our/example/furniture/controller/FurnitureController.java
@@ -4,7 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
 import our.example.furniture.dto.PostWriterDto;
+import our.example.furniture.dto.RegisterDto;
 
 @Controller
 public class FurnitureController {
@@ -18,11 +20,14 @@ public class FurnitureController {
     public String postWriter(Model model) {
         return "postWriter";
     }
-    // register[회원가입] :: URL 매핑
-    @GetMapping("/register")
-    public String register(Model model) {
-        return "register";
-    }
+
+	 // register[회원가입] :: URL 매핑
+	 @GetMapping("/register")
+	 public String register(RegisterDto registerDto) {
+	 System.out.println(registerDto.getRegisterPhone());
+	 return "register";
+	 }
+	 
     // postWriter[게시글작성] :: URL 매핑
     @PostMapping("/productRegister")
     public String temp(PostWriterDto postWriterDto) {
