321da3d2b9ffe80dd5d48534346f24733b720b7a
wootta-1559 <wootta1559@naver.com>
2021-01-15 00:01:02 +0900

    로그인 로직

--- src/main/java/our/example/furniture/controller/LoginController.java
+++ src/main/java/our/example/furniture/controller/LoginController.java
@@ -1,10 +1,13 @@
 package our.example.furniture.controller;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import our.example.furniture.dto.UserRegisterDto;
 import our.example.furniture.repository.LoginMapper;
@@ -13,6 +16,7 @@ import our.example.furniture.repository.LoginMapper;
 public class LoginController {
 	@Autowired
     private LoginMapper loginMapper;
+	private Log log = LogFactory.getLog(this.getClass());
 
 // login[로그인] :: URL 매핑
     @GetMapping("/login")
@@ -20,10 +24,11 @@ public class LoginController {
     	return "login";
     }
     
+    @ResponseBody
     @PostMapping("/loginSuccess")
     public String temp(UserRegisterDto loginDto) {
-    	loginMapper.overlapLogin(loginDto);
-    	loginMapper.overlapPassword(loginDto);
-    	return "index";
+    	String data = loginMapper.overlapLogin(loginDto);
+    	log.info(loginDto.getuserRegisterId());
+    	return data;
     }
 }
---
+++ src/main/java/our/example/furniture/service/Login.java
@@ -0,0 +1,9 @@
+package our.example.furniture.service;
+
+import org.springframework.stereotype.Repository;
+
+//해당클래스를 bean 객체로 선언해주는 annotation
+@Repository
+public class Login {
+	
+}
