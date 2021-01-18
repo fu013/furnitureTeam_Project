c664a93c6f52915b0f3358e92c517f37b3105ff4
seungchan <fu017@naver.com>
2021-01-18 20:08:51 +0900

    로그아웃 / 로그인 헤더 구현

--- src/main/java/our/example/furniture/controller/LoginController.java
+++ src/main/java/our/example/furniture/controller/LoginController.java
@@ -9,11 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import org.springframework.web.servlet.ModelAndView;
 import our.example.furniture.dto.UserRegisterDto;
 import our.example.furniture.repository.LoginMapper;
 
+import javax.script.ScriptContext;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @Controller
 public class LoginController {
@@ -27,6 +32,7 @@ public class LoginController {
         return "login";
     }
 
+    // 로그인 요청
     @ResponseBody
     @PostMapping("/loginSuccess")
     public String loginSuccess(UserRegisterDto userRegisterDto, HttpServletRequest request, Model model) {
@@ -35,6 +41,7 @@ public class LoginController {
             String userName = userRegisterDto.getUserRegisterId();
             HttpSession session = request.getSession();
             session.setAttribute("loginUser", userName);
+            session.setMaxInactiveInterval(60*60);
         /*
             String name = (String) session.getAttribute("loginUser");
             log.info("세션에 로그인 되었습니다.");
@@ -44,5 +51,17 @@ public class LoginController {
             log.info("아이디나 비밀번호가 맞지않아 응답값이 Null 입니다.");
         }
         return result;
+    }
+    // 로그아웃 요청
+    @GetMapping("/logout")
+    public void logout(HttpSession session, HttpServletResponse response) throws IOException {
+        session.invalidate();
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter out = response.getWriter();
+        out.println("<script>");
+        out.println("alert('로그아웃되었습니다.');");
+        out.println("location.href=document.referrer;");
+        out.println("</script>");
+        out.close();
     }
 }
