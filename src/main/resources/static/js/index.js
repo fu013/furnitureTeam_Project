d057d86db0d5035208ac30b112e307a60b0c99a8
seungchan <fu017@naver.com>
2021-02-09 03:41:48 +0900

    가격 세자리수 마다 [,] 찍게 수정하고, 검색쪽 css 살짝 변경

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -33,4 +33,29 @@ $(document).ready(function () {
             $(`.${paramSearchType}`).addClass('on');
         }
 
+        // 숫자 타입에서 쓸 수 있도록 format() 함수 추가
+        Number.prototype.format = function(){
+            if(this==0) return 0;
+
+            var reg = /(^[+-]?\d+)(\d{3})/;
+            var n = (this + '');
+
+            while (reg.test(n)) n = n.replace(reg, '$1' + ',' + '$2');
+
+            return n;
+        };
+
+        // 문자열 타입에서 쓸 수 있도록 format() 함수 추가
+        String.prototype.format = function(){
+            var num = parseFloat(this);
+            if( isNaN(num) ) return "0";
+
+            return num.format();
+        };
+
+        var uiProductPrice = document.getElementsByClassName("uiProductPrice");
+        for (var i = 0; i < uiProductPrice.length; i++) {
+           var string = uiProductPrice[i].innerHTML;
+           uiProductPrice[i].innerHTML = "가격 : " + string.format() + " 원";
+        }
 });
